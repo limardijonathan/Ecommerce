50cd4c50c752b42734e28818d667dd68db243397
SolMalnis <solmalnis@gmail.com>
2021-09-20 19:35:47 -0300

    express y html login

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require("express")
+
+const app =express()
+
+let path = require('path')
+
+const publicPath = path.join(__dirname,'./public')
+app.use(express.static(publicPath))
+
+
+app.listen(5050, ()=>{
+    console.log('funciona en http://localhost:5050/login' )
+})
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
+
