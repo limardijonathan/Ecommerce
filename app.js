a8f6185b90deb08fd188e8071590b490c2554aa6
SolMalnis <solmalnis@gmail.com>
2021-09-26 17:39:27 -0300

    registro v1 carrito v1

--- app.js
+++ app.js
@@ -9,10 +9,30 @@ app.use(express.static(publicPath))
 
 
 app.listen(5050, ()=>{
-    console.log('funciona en http://localhost:5050/login' )
+    console.log('funciona en http://localhost:5050/productCart' )
 })
 
 app.get('/login', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+
+app.get('/productCart', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/productCart.html'))
+})
