d8a3cdf2b096d5602159c39600b7a235003bd6fc
jlimardi <limardi.jonathan@gmail.com>
2021-09-26 16:20:33 -0300

    Home - Footer - Header (Sin MOBILE)  v.1.0

--- app.js
+++ app.js
@@ -16,3 +16,6 @@ app.get('/login', (req,res) =>{
     res.sendFile(path.join(__dirname,'/views/login.html'))
 })
 
+app.get('/home', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
