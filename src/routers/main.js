cae07a99bacae942e44bb2ee77a710d5beadf6ae
jlimardi <limardi.jonathan@gmail.com>
2021-11-22 19:50:13 -0300

    Profile v1.0

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,11 +1,9 @@
-
-
+const express=require("express");
 const router= express.Router();
-
 const mainControler = require("../controllers/mainController");
 
 
-router.get('/',mainController.home)
+router.get('/',mainControler.home)
 
 
 
