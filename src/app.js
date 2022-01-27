2fa5407794db0f2a638363bb768b625a05439813
SolMalnis <solmalnis@gmail.com>
2022-01-27 20:17:22 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ const apiProductRouter= require('./routers/api/products')
 const apiUserRouter= require('./routers/api/users')
 const methodOverride=require("method-override")
 const cookie = require("cookie-parser")
+const cors = require('cors')
+
 
 
 // ************ express()************
@@ -29,6 +31,7 @@ app.use(express.static(publicPath))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use (methodOverride("_method"))
+app.use(cors())
 
 app.use(session({
     secret: "Shh, it's a secret",
